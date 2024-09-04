68215642ae64e744f3b3324ec9e464b9f02f9f45
sungjinw04 <abhishekpandit4224@gmail.com>
2024-09-04 20:09:24 +0530

    Update database.py

--- Bot/utils/database.py
+++ Bot/utils/database.py
@@ -4,3 +4,9 @@ async def get_active_chats():
     # Example implementation
     return []
 
+# Bot/utils/database.py
+
+async def get_authuser_names(chat_id):
+    # Example implementation
+    return ["user1", "user2"]
+
