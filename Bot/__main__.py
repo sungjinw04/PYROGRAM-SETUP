334548a17c2097e3964127416fe8a780f62e19aa
sungjinw04 <abhishekpandit4224@gmail.com>
2024-09-05 02:39:00 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -9,9 +9,11 @@ from pyrogram import idle
 loop = asyncio.get_event_loop()
 IMPORTED = {}
 
+
 LOG = Console()
 
 async def main():
+    # from .modules.waifu_dropper import character_cache
     LOG.print(f"[bold yellow]Loading {len(MODULES_PATH)} Modules")
     for module in MODULES_PATH:
         mod = module.replace(os.getcwd(),"")[1:].replace('/','.').replace(".py",'')
@@ -19,11 +21,21 @@ async def main():
         
         importlib.import_module(mod)
 
-    print("✨ Bot started")
+    # if os.path.exists("./waifu_cache.json"):
+    #     with open("./waifu_cache.json", "r") as f:
+    #         data = json.load(f)
+    #         print(data)
+    #         character_cache.update(data)
+    #         os.remove("./waifu_cache.json")
+          
+    print("✨ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ")
 
     await idle()
-    print("Cancelling all tasks.")
+    # with open("./waifu_cache.json", "w") as file:
+    #     json.dump(character_cache, file)
+    print("ᴄᴀɴᴄᴇʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
+
 
 if __name__ == "__main__":
     loop.run_until_complete(main())
-
+    
