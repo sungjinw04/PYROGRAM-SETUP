9c0f357ac86eacd4648682c3786066f3c0c1afc6
sungjinw04 <abhishekpandit4224@gmail.com>
2024-09-04 20:01:09 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,12 @@
 # config.py
+
 api_id = "25395782"
 api_hash = "3c0f6066a07142d664690cfd34447450"
 bot_token = "7329929698:AAGD5Ccwm0qExCq9_6GVHDp2E7iidLH-McU"
 MONGO_URL = "mongodb+srv://misamusic:misamusic@cluster0.s9trzk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 OWNER_ID = 1886390680
 SUPPORT_CHAT_ID = -1002190382416
+
+# Define adminlist (assuming it's a list of chat admins or something similar)
+adminlist = {}  # Initialize as an empty dictionary
+
